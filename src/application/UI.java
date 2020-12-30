1b516fe130cc2ae128c67d202f3bf21a41c529c4
Eliton Peroni <eliton_peroni@hotmail.com>
2020-12-30 19:27:02 -0300

    Moving Pieces

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,11 +1,8 @@
 package application;
 
 import java.util.InputMismatchException;
-import java.util.List;
 import java.util.Scanner;
 
-
-import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.Color;
@@ -33,12 +30,6 @@ public class UI {
     public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
-    // https://stackoverflow.com/questions/2979383/java-clear-the-console
-    public static void clearScreen() {
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
-    }
-
     public static ChessPosition readChessPosition(Scanner sc) {
         try {
             String s = sc.nextLine();
@@ -51,35 +42,20 @@ public class UI {
         }
     }
 
-
     public static void printBoard(ChessPiece[][] pieces) {
         for (int i = 0; i < pieces.length; i++) {
             System.out.print((8 - i) + " ");
             for (int j = 0; j < pieces.length; j++) {
-                printPiece(pieces[i][j], false);
+                printPiece(pieces[i][j]);
             }
             System.out.println();
         }
         System.out.println("  a b c d e f g h");
     }
 
-    public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
-        for (int i = 0; i < pieces.length; i++) {
-            System.out.print((8 - i) + " ");
-            for (int j = 0; j < pieces.length; j++) {
-                printPiece(pieces[i][j], possibleMoves[i][j]);
-            }
-            System.out.println();
-        }
-        System.out.println("  a b c d e f g h");
-    }
-
-    private static void printPiece(ChessPiece piece, boolean background) {
-        if (background) {
-            System.out.print(ANSI_BLUE_BACKGROUND);
-        }
+    private static void printPiece(ChessPiece piece) {
         if (piece == null) {
-            System.out.print("-" + ANSI_RESET);
+            System.out.print("-");
         }
         else {
             if (piece.getColor() == Color.WHITE) {
@@ -91,6 +67,4 @@ public class UI {
         }
         System.out.print(" ");
     }
-
-
 }
--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -29,25 +29,40 @@ public class Board {
         }
         return pieces[row][column];
     }
+
     public Piece piece(Position position) {
         if (!positionExists(position)) {
             throw new BoardException("Position not on the board");
         }
         return pieces[position.getRow()][position.getColumn()];
     }
+
     public void placePiece(Piece piece, Position position) {
         if (thereIsAPiece(position)) {
-            throw new BoardException("There is already a piece on position" + position);
+            throw new BoardException("There is already a piece on position " + position);
         }
         pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
+    }
+
+    public Piece removePiece(Position position) {
+        if (!positionExists(position)) {
+            throw new BoardException("Position not on the board");
+        }
+        if (piece(position) == null) {
+            return null;
+        }
+        Piece aux = piece(position);
+        aux.position = null;
+        pieces[position.getRow()][position.getColumn()] = null;
+        return aux;
     }
 
     private boolean positionExists(int row, int column) {
         return row >= 0 && row < rows && column >= 0 && column < columns;
     }
 
-    public boolean  positionExists(Position position) {
+    public boolean positionExists(Position position) {
         return positionExists(position.getRow(), position.getColumn());
     }
 
@@ -55,7 +70,6 @@ public class Board {
         if (!positionExists(position)) {
             throw new BoardException("Position not on the board");
         }
-
         return piece(position) != null;
     }
 }
